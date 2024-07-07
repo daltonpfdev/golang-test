f90ba5ad0e270d2de4febcc3bf04836e9a7f3c6c
daltonpfdev <daltonpf.dev@gmail.com>
2024-07-07 13:28:49 -0300

    add custom logger

---
+++ config/config.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"gorm.io/gorm"
+)
+
+var (
+	db     *gorm.DB
+	logger *Logger
+)
+
+func InitConfig() error {
+	return nil
+}
+
+func GetLogger(p string) *Logger {
+	logger = NewLogger(p)
+	return logger
+}
--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/daltonpfdev/golang-test/router"
+import (
+	"github.com/daltonpfdev/golang-test/config"
+	"github.com/daltonpfdev/golang-test/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Iniciando as Configurações
+	err := config.InitConfig()
+	if err != nil {
+		logger.Errf("Erro na Inicialização das Configurações: %v", err)
+		return
+	}
+
 	// Iniciando o Router
 	router.Inicializador()
 }
